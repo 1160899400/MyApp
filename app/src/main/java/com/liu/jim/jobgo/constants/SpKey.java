efbe95428b84622ecbbe70c6a8135e1be3ec9e0c
jim <1160899400@qq.com>
2019-01-27 13:16:37 +0800

    add preference constant

---
+++ app/src/main/java/com/liu/jim/jobgo/constants/SpKey.java
@@ -0,0 +1,25 @@
+package com.liu.jim.jobgo.constants;
+
+/**
+ * @author Jim.Liu
+ * liu13407134075@gmail.com
+ *
+ * Key of SharedPreferences
+ *
+ */
+
+
+public interface SpKey {
+
+    /**
+     * 上次登录的token，用于检测用户登录状态
+     */
+    String LAST_LOGIN_TOKEN = "lt";
+
+
+
+
+
+
+
+}
